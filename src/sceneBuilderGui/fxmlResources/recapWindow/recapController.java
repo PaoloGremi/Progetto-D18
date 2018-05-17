a0edf96a8069937557dfa8032d9c1070fc9c5e23
beard <andrea.guzzon33@protonmail.com>
2018-05-17 15:32:17 +0200

    Added recap window and controller sample

---
+++ src/sceneBuilderGui/fxmlResources/recapWindow/recapController.java
@@ -0,0 +1,33 @@
+package sceneBuilderGui.fxmlResources.recapWindow;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class recapController {
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    private TextField totalPrice;
+
+    @FXML
+    private TextField totalBudget;
+
+    @FXML
+    private ListView<?> createdMenu;
+
+    @FXML
+    private ListView<?> menuDetails;
+
+    @FXML
+    private TextField guestsNumber;
+
+    @FXML
+    private TextField menuPrice;
+
+    @FXML
+    private Button back;
+
+}
